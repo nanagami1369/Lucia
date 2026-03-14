8fac719805ea33fbc4eabd20ac14af72e0c8392c
nanagami1369 <nanagami1369@gmail.com>
2026-03-15 00:14:56 +0900

    確認モーダルを作成

---
+++ src/Lucia.Server/Lucia.Server.Client/Components/Modals/ConfirmOption.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Lucia.Server.Client.Components.Modals;
+
+/// <summary>
+/// 確認ダイアログのオプション
+/// </summary>
+public class ConfirmOptions {
+
+    /// <summary>確認種別</summary>
+    public required ConfirmType Type { get; init; }
+
+    /// <summary>タイトル（省略可）</summary>
+    public string? Title { get; init; }
+
+    /// <summary>本文メッセージ</summary>
+    public required string Message { get; init; }
+
+    /// <summary>OKボタンのラベル</summary>
+    public required string OkMessage { get; init; }
+
+    /// <summary>キャンセルボタンのラベル</summary>
+    public required string CancelMessage { get; init; }
+
+}
---
+++ src/Lucia.Server/Lucia.Server.Client/Components/Modals/ConfrimType.cs
@@ -0,0 +1,46 @@
+﻿namespace Lucia.Server.Client.Components.Modals;
+
+/// <summary>
+/// 確認ダイアログの種別
+/// </summary>
+public enum ConfirmType {
+    /// <summary>情報</summary>
+    Info,
+    /// <summary>警告</summary>
+    Warm,
+    /// <summary>危険</summary>
+    Danger
+}
+
+/// <summary>
+/// <see cref="ConfirmType"/> の拡張メソッド
+/// </summary>
+public static class ConfirmTypeExtensions {
+
+    /// <summary>
+    /// タイトルなしの <see cref="ConfirmOptions"/> を生成する
+    /// </summary>
+    /// <param name="type">確認種別</param>
+    /// <param name="message">本文メッセージ</param>
+    /// <param name="okMessage">OKボタンのラベル</param>
+    /// <param name="cancelMessage">キャンセルボタンのラベル</param>
+    public static ConfirmOptions Create(this ConfirmType type, string message, string okMessage, string cancelMessage) {
+        return new ConfirmOptions {
+            Type = type, Message = message, OkMessage = okMessage, CancelMessage = cancelMessage
+        };
+    }
+
+    /// <summary>
+    /// タイトルありの <see cref="ConfirmOptions"/> を生成する
+    /// </summary>
+    /// <param name="type">確認種別</param>
+    /// <param name="title">タイトル</param>
+    /// <param name="message">本文メッセージ</param>
+    /// <param name="okMessage">OKボタンのラベル</param>
+    /// <param name="cancelMessage">キャンセルボタンのラベル</param>
+    public static ConfirmOptions Create(this ConfirmType type, string title, string message, string okMessage, string cancelMessage) {
+        return new ConfirmOptions {
+            Type = type, Title = title, Message = message, OkMessage = okMessage, CancelMessage = cancelMessage
+        };
+    }
+}
