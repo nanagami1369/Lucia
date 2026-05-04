39daa2b4a21be8db3cc528e4aa8301aac98b2bb6
nanagami1369 <nanagami1369@gmail.com>
2026-05-04 10:36:08 +0900

    --versionのような管理者権限が不要なコマンドは管理者権限を取得せずに実行できるように改修

--- src/Lucia.Installer/Program.cs
+++ src/Lucia.Installer/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Security.Principal;
 using System.Text;
 using ConsoleAppFramework;
 using Lucia.Installer;
@@ -21,6 +23,29 @@ if (!knownCommands.Contains(args[0]))
     Console.Error.WriteLine("使い方を確認するには: Lucia.Installer.exe --help");
     Environment.Exit(1);
     return;
+}
+
+// 管理者権限が必要なコマンドは、権限不足の場合に runas で自己再起動して UAC プロンプトを表示する
+string[] requiresAdminCommands = ["install", "uninstall", "modify"];
+if (requiresAdminCommands.Contains(args[0]))
+{
+    var identity = WindowsIdentity.GetCurrent();
+    var principal = new WindowsPrincipal(identity);
+    if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+    {
+        var currentExePath = Environment.ProcessPath
+            ?? throw new InvalidOperationException("現在のプロセスパスを取得できませんでした。");
+        var quotedArgs = args.Select(arg => arg.Contains(' ') ? $"\"{arg}\"" : arg);
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = currentExePath,
+            Arguments = string.Join(" ", quotedArgs),
+            Verb = "runas",
+            UseShellExecute = true,
+        };
+        Process.Start(processStartInfo);
+        return;
+    }
 }
 
 var app = ConsoleApp.Create();
